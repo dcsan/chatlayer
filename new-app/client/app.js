83b3e1f16e864a6bf14ea4c919d70abbe88b8685
dcsan <dc@pikkle.com>
2015-02-27 22:20:48 -0800

    basic GA integration

--- new-app/client/app.js
+++ new-app/client/app.js
@@ -1,9 +1,9 @@
 Template.content.rendered = function () {
 
-      window.onscroll = function() {
-          var speed = 3.5;
-          document.body.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
-        }
+  window.onscroll = function() {
+    var speed = 3.5;
+    document.body.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
+  }
 };
 
 Template.content.helpers({
