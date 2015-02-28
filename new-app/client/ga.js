5f1bc19e7753a17523ea3599db333dced3e8fab5
dcsan <dc@pikkle.com>
2015-02-27 22:38:38 -0800

    ga code not running locally

--- new-app/client/ga.js
+++ new-app/client/ga.js
@@ -1,4 +1,15 @@
 
+Meteor.startup(function () {
+
+  // dont send events from dev mode
+  if (window.location.host.includes("localhost")) {
+    console.log("skipping localhost");
+    window.ga = {}
+    return;
+  } else {
+    console.log("init GA")
+  }
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,4 +17,16 @@
 
   ga('create', 'UA-60202659-1', 'auto');
   ga('send', 'pageview');
+
+  window.ga = ga;
+
+  // global event
+  window.gaping = function(params) {
+    if (typeof ga=="function") {
+      ga('send', 'pageview');
+    }
+  }
+
+});
+
 
