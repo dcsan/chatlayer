5745e2530491f0369c6319d38970a59601f2b6bb
Main <main@Coffees-MacBook-Pro.local>
2015-02-28 15:29:00 -0800

    Updates

--- new-app/client/app.js
+++ new-app/client/app.js
@@ -49,3 +49,37 @@ Template.add.events({
     Requests.insert(data);
   }
 });
+Template.chatroom.helpers({
+  msg: function () {
+    return Messages.find({}, {sort: { time: -1}})
+  }
+});
+
+Template.chatroom.events({
+  'submit': function (e, t) {
+
+    e.preventDefault();
+
+    var text  = $("#message").val();
+
+    var user  = Meteor.user().profile.name;
+    var id    = Meteor.user().services.github.id;
+    var user  = Meteor.user().services.github.username;
+
+    if (text === "") {
+      alert("Please enter your request.")
+      return
+    } 
+
+    var data = {
+      'user':    id,
+      'name':    user,
+      'message': text,
+      'username': user,
+      'time':    new Date()
+    }
+
+    Messages.insert(data);
+    $("#message").val("");
+  }
+});
--- new-app/collections/collections.js
+++ new-app/collections/collections.js
@@ -1,1 +1,2 @@
 Requests = new Mongo.Collection("requests");
+Messages = new Mongo.Collection("messages");
