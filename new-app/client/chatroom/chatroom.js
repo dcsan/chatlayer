8411a619151c18c1e71dc08b35112f2dee92c872
dcsan <dc@pikkle.com>
2015-02-28 19:23:43 -0800

    popups location

--- new-app/client/chatroom/chatroom.js
+++ new-app/client/chatroom/chatroom.js
@@ -29,8 +29,11 @@ Template.chatroom.events({
     }
 
     Requests.insert(data);
+    Map.addBubble(data);
     console.log("added", data)
   }
+
+
 });
 Template.chatroom.helpers({
   msg: function () {
@@ -38,31 +41,3 @@ Template.chatroom.helpers({
   }
 });
 
-Template.chatroom.events({
-  'submit': function (e, t) {
-
-    e.preventDefault();
-
-    var text  = $("#message").val();
-
-    var user  = Meteor.user().profile.name;
-    var id    = Meteor.user().services.github.id;
-    var user  = Meteor.user().services.github.username;
-
-    if (text === "") {
-      alert("Please enter your request.")
-      return
-    } 
-
-    var data = {
-      'user':    id,
-      'name':    user,
-      'message': text,
-      'username': user,
-      'time':    new Date()
-    }
-
-    Messages.insert(data);
-    $("#message").val("");
-  }
-});
