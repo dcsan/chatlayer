fe999e43d76604ba641d91971bdeaaa6f90bd85f
dcsan <dc@pikkle.com>
2015-03-01 12:01:35 -0800

    html redux

--- new-app/client/content/content.js
+++ new-app/client/content/content.js
@@ -1,7 +1,7 @@
-Template.content.rendered = function () {
+// Template.content.rendered = function () {
 
-  window.onscroll = function() {
-    var speed = 3.5;
-    document.body.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
-  }
-};
+//   window.onscroll = function() {
+//     var speed = 3.5;
+//     document.body.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
+//   }
+// };
--- new-app/client/messages/messages.js
+++ new-app/client/messages/messages.js
@@ -1,28 +1,28 @@
-Template.messages.events({
-  'submit': function (e, t) {
+// Template.messages.events({
+//   'submit': function (e, t) {
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    var text  = $("#message").val();
+//     var text  = $("#message").val();
 
-    var user  = Meteor.user().profile.name;
-    var id    = Meteor.user().services.github.id;
-    var user  = Meteor.user().services.github.username;
+//     var user  = Meteor.user().profile.name;
+//     var id    = Meteor.user().services.github.id;
+//     var user  = Meteor.user().services.github.username;
 
-    if (text === "") {
-      alert("Please enter your request.")
-      return
-    } 
+//     if (text === "") {
+//       alert("Please enter your request.")
+//       return
+//     } 
 
-    var data = {
-      'user':    id,
-      'name':    user,
-      'message': text,
-      'username': user,
-      'time':    new Date()
-    }
+//     var data = {
+//       'user':    id,
+//       'name':    user,
+//       'message': text,
+//       'username': user,
+//       'time':    new Date()
+//     }
 
-    Messages.insert(data);
-    $("#message").val("");
-  }
-});
+//     Messages.insert(data);
+//     $("#message").val("");
+//   }
+// });
