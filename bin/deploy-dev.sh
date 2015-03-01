0272d2ff7301b7a669cfcfc94a7ad928bb876591
dcsan <dc@pikkle.com>
2015-02-28 22:38:34 -0800

    deploy scripts

--- bin/deploy-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env
-cd app
-meteor deploy --settings private/prod.json backchannel-dev.meteor.com
---
+++ bin/deploy-staging.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env
+cd new-app
+meteor deploy --settings private/staging.json backchannel-dev.meteor.com
